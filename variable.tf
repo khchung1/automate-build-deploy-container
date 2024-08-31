8e085039c0d144b52ea8bdce4e978f522925c4fc
khchung1 <khchung87@gmail.com>
2024-09-01 00:21:05 +0800

    second commit

--- variable.tf
+++ variable.tf
@@ -1,5 +1,6 @@
 #using $GITHUB_ENV, Terraform will automatically recognize and use it as an input variable. 
 variable "repo_uri_variable" {
   type = string
+  default = "public.ecr.aws/u2q1a2y8/khchung/simple-node-app"
   description = "The repository URI from ECR" 
 }
